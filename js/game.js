fe27b5a4eb587bdefa1e9ae39136fdfe39fa0024
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-05-07 14:48:44 +0000

    Made screen scale properly

--- js/game.js
+++ js/game.js
@@ -1,9 +1,23 @@
 /*global Phaser*/
+// Game Scene
 const config = {
   type: Phaser.AUTO,
   width:1920,
   height:1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  //Set background colour
   backgroundColor: "#000000",
+  //Set Scaling for the site
+  scale: {
+    mode: Phaser.Scale.FIT,
+    //Set Position to middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
