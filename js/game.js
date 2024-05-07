9fdd120cf81aa166eb8b242d760e55d54293e15c
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-05-07 14:23:54 +0000

    Updated repository

--- js/game.js
+++ js/game.js
@@ -1,7 +1,10 @@
 /*global Phaser*/
-const Config = {
+const config = {
   type: Phaser.AUTO,
   width:1920,
   height:1080,
   backgroundColor: "#000000",
 }
+
+const game = new Phaser.Game(config)
+console.log(game)
