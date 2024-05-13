af0c515c67930fc541766d2f0b1ccbded4b98eb9
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-05-13 14:56:26 +0000

    finished step 6

--- js/splashScene.js
+++ js/splashScene.js
@@ -7,14 +7,14 @@ class SplashScene extends Phaser.Scene{
   }
   preload(){
     //Loading Images
-    this.load.video('splashSceneBackground','/assets/IMHSplashScene.mp4')
+    this.load.video('splashSceneBackground','./assets/IMHSplashScene.mp4')
   }
   create(data){
     //Load titlescreen
-    this.splashSceneBackgroundImage = this.add.video(0,0,'splashSceneBackground')
-    this.splashSceneBackgroundImage.play(false);
-    this.splashSceneBackgroundImage.x=1920/2
-    this.splashSceneBackgroundImage.y=1080/2
+    this.splashSceneBackgroundVideo = this.add.video(0,0,'splashSceneBackground')
+    this.splashSceneBackgroundVideo.play(false);
+    this.splashSceneBackgroundVideo.x=1920/2
+    this.splashSceneBackgroundVideo.y=1080/2
   }
   update(time,delta){
     if (time>5000){
--- js/titleScene.js
+++ js/titleScene.js
@@ -2,14 +2,25 @@ class TitleScene extends Phaser.Scene {
   constructor(){
     super({key:'titleScene'})
     this.titleSceneBackgroundImage = null;
+    this.titleSceneText = null;
   }
   init(data){
     this.cameras.main.setBackgroundColor('#000000')
   }
   preload(){
     console.log('Title Scene') 
+    this.load.image('titleSceneBackground','./assets/aliens_screen_image.jpg')
+    this.load.image('titleSceneText','./assets/SuperShootersTitleText.png')
   }
   create(data){
+    //Background
+    this.titleSceneBackgroundImage = this.add.sprite(0,0,'titleSceneBackground').setScale(2.75)
+    this.titleSceneBackgroundImage.x = 1920/2
+    this.titleSceneBackgroundImage.y = 1080/2
+    //Text
+    this.titleSceneBackgroundImage = this.add.sprite(0,0,'titleSceneText').setScale(1.25)
+    this.titleSceneBackgroundImage.x = 1920/4
+    this.titleSceneBackgroundImage.y = 1080/3
   }
   update(time,delta){
   }
