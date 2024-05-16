8e84c7bf8a64d9aa834dc86235f8a8f64614aef6
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-05-16 18:47:29 +0000

    Scrolling background and more

--- js/gameScene.js
+++ js/gameScene.js
@@ -2,9 +2,12 @@ class GameScene extends Phaser.Scene {
   constructor(){
     super({key:'gameScene'})
     this.background = null;
+    this.background2 = null;
+    this.background3 = null;
     this.ship = null;
     this.shipVel = 0;
     this.fireMissile=false
+    this.currentzindex = -1;
   }
   init(data){
     this.cameras.main.setBackgroundColor('#000000')
@@ -17,10 +20,17 @@ class GameScene extends Phaser.Scene {
   }
   create(data){
     //background
-    this.background = this.add.image(0,0,'starBackground').setScale(2.0)
-    this.background.setOrigin(0,0)
+    this.background = this.add.image(0,0,'starBackground').setScale(2)
+    this.background.x = 540*6
+    this.background.y = 720
+    this.background2 = this.add.image(0,0,'starBackground').setScale(2)
+    this.background2.x = 540*4
+    this.background2.y = 720
+    this.background3 = this.add.image(0,0,'starBackground').setScale(2)
+    this.background3.x = 540*1
+    this.background3.y = 720
     //ship
-    this.ship = this.physics.add.sprite(225,1080/2,'ship').setScale(5.0)
+    this.ship = this.physics.add.sprite(225,1080/2,'ship').setScale(0.7)
     //misslegroup
     this.missileGroup = this.physics.add.group()
   }
@@ -42,6 +52,8 @@ class GameScene extends Phaser.Scene {
     } else{
       this.shipVel=clampNumber(this.shipVel*.9,-9,9)
     }
+    //SettingShipDepth
+    this.ship.setDepth(5);
     //Move ship by velocity
     this.ship.y+=this.shipVel
     //Rotate ship
@@ -55,14 +67,36 @@ class GameScene extends Phaser.Scene {
     //Missiles
     if (MissileKey.isDown===true){
       if (this.fireMissile == false){
+        let LastFired = time;
         //fire Missile
         this.fireMissile=true
-        const NewMissile = this.physics.add.sprite(this.ship.x,this.ship.y,'missile').setScale(2)
+        const NewMissile = this.physics.add.sprite(this.ship.x+20,this.ship.y,'missile').setScale(.3)
         NewMissile.rotation = Phaser.Math.DegToRad(90);
         this.missileGroup.add(NewMissile)
       }
     } else {
       this.fireMissile=false
+    }
+
+    //Scrolling background
+    this.background.x-=10
+    this.background2.x-=10
+    this.background3.x-=10
+    //Checking if backgrounds have moved too far back
+    if (this.background.x<-980){
+      this.background.x=540*6
+      this.currentzindex-=1
+      this.background.setDepth(this.currentzindex);
+    }
+    if (this.background2.x<-980){
+      this.background2.x=540*6
+      this.currentzindex-=1
+      this.background2.setDepth(this.currentzindex);
+    }
+    if (this.background3.x<-980){
+      this.background3.x=540*6
+      this.currentzindex-=1
+      this.background3.setDepth(this.currentzindex);
     }
   }
   RotateShip(degree) {
