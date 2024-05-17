7c3d64595238bd664cdf0e93da9880ee603a5e57
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-05-17 01:35:05 +0000

    Missile Smoke Particles

--- js/gameScene.js
+++ js/gameScene.js
@@ -1,42 +1,44 @@
 class GameScene extends Phaser.Scene {
-  constructor(){
-    super({key:'gameScene'})
+  constructor() {
+    super({ key: 'gameScene' })
     this.background = null;
     this.background2 = null;
     this.background3 = null;
     this.ship = null;
     this.shipVel = 0;
-    this.fireMissile=false
+    this.fireMissile = false
     this.currentzindex = -1;
   }
-  init(data){
+  init(data) {
     this.cameras.main.setBackgroundColor('#000000')
   }
-  preload(){
+  preload() {
     //images
-    this.load.image('starBackground','./assets/starBackground.png')
-    this.load.image('ship','./assets/spaceShip.png')
-    this.load.image('missile','/assets/missile.png')
+    this.load.image('starBackground', './assets/starBackground.png')
+    this.load.image('ship', './assets/spaceShip.png')
+    this.load.image('missile', '/assets/missile.png')
+    this.load.image('smoke', '/assets/SmokePixel.png')
     //sound
-    this.load.audio('missile','./assets/missile.mp3')
+    this.load.audio('missile', './assets/missile.mp3')
   }
-  create(data){
+  create(data) {
     //background
-    this.background = this.add.image(0,0,'starBackground').setScale(2)
-    this.background.x = 540*6
+    this.background = this.add.image(0, 0, 'starBackground').setScale(2)
+    this.background.x = 540 * 6
     this.background.y = 720
-    this.background2 = this.add.image(0,0,'starBackground').setScale(2)
-    this.background2.x = 540*4
+    this.background2 = this.add.image(0, 0, 'starBackground').setScale(2)
+    this.background2.x = 540 * 4
     this.background2.y = 720
-    this.background3 = this.add.image(0,0,'starBackground').setScale(2)
-    this.background3.x = 540*1
+    this.background3 = this.add.image(0, 0, 'starBackground').setScale(2)
+    this.background3.x = 540 * 1
     this.background3.y = 720
     //ship
-    this.ship = this.physics.add.sprite(225,1080/2,'ship').setScale(0.7)
+    this.ship = this.physics.add.sprite(225, 1080 / 2, 'ship').setScale(0.7)
     //misslegroup
     this.missileGroup = this.physics.add.group()
+    this.smokeGroup = this.physics.add.group()
   }
-  update(time,delta){
+  update(time, delta) {
     //updates 60 times a second
     //Keybinds for movement (2 for up 2 for down depending on playstyle)
     const LeftKey = this.input.keyboard.addKey('W')
@@ -47,60 +49,82 @@ class GameScene extends Phaser.Scene {
     //Function to clamp the speed, so its fixed within a certain ranges
     const clampNumber = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
     //Detect movement, and change velocities
-    if (LeftKey.isDown==true||LeftKey2.isDown==true){
-      this.shipVel=clampNumber(this.shipVel-2,-9,9)
-    } else if (RightKey.isDown==true||RightKey2.isDown==true){
-      this.shipVel=clampNumber(this.shipVel+2,-9,9)
-    } else{
-      this.shipVel=clampNumber(this.shipVel*.9,-9,9)
+    if (LeftKey.isDown == true || LeftKey2.isDown == true) {
+      this.shipVel = clampNumber(this.shipVel - 2, -9, 9)
+    } else if (RightKey.isDown == true || RightKey2.isDown == true) {
+      this.shipVel = clampNumber(this.shipVel + 2, -9, 9)
+    } else {
+      this.shipVel = clampNumber(this.shipVel * .9, -9, 9)
     }
     //SettingShipDepth
     this.ship.setDepth(5);
     //Move ship by velocity
-    this.ship.y+=this.shipVel
+    this.ship.y += this.shipVel
     //Rotate ship
-    this.RotateShip(90+this.shipVel*2)
+    this.RotateShip(90 + this.shipVel * 2)
     //Wrapping
-    if (this.ship.y<0){
+    if (this.ship.y < 0) {
       this.ship.y = 1080
-    } else if (this.ship.y>1080){
+    } else if (this.ship.y > 1080) {
       this.ship.y = 0
     }
     //Missiles
-    if (MissileKey.isDown===true){
-      if (this.fireMissile == false){
-        let LastFired = time;
+    if (MissileKey.isDown === true) {
+      if (this.fireMissile == false) {
         //fire Missile
-        this.fireMissile=true
-        const NewMissile = this.physics.add.sprite(this.ship.x+20,this.ship.y,'missile').setScale(.3)
+        this.fireMissile = true
+        const NewMissile = this.physics.add.sprite(this.ship.x + 20, this.ship.y, 'missile').setScale(.3)
         NewMissile.rotation = Phaser.Math.DegToRad(90);
         this.missileGroup.add(NewMissile)
         this.sound.play('missile')
       }
     } else {
-      this.fireMissile=false
+      this.fireMissile = false
     }
 
     //Scrolling background
-    this.background.x-=10
-    this.background2.x-=10
-    this.background3.x-=10
+    this.background.x -= 10
+    this.background2.x -= 10
+    this.background3.x -= 10
     //Checking if backgrounds have moved too far back
-    if (this.background.x<-980){
-      this.background.x=540*6
-      this.currentzindex-=1
+    if (this.background.x < -980) {
+      this.background.x = 540 * 6
+      this.currentzindex -= 1
       this.background.setDepth(this.currentzindex);
     }
-    if (this.background2.x<-980){
-      this.background2.x=540*6
-      this.currentzindex-=1
+    if (this.background2.x < -980) {
+      this.background2.x = 540 * 6
+      this.currentzindex -= 1
       this.background2.setDepth(this.currentzindex);
     }
-    if (this.background3.x<-980){
-      this.background3.x=540*6
-      this.currentzindex-=1
+    if (this.background3.x < -980) {
+      this.background3.x = 540 * 6
+      this.currentzindex -= 1
       this.background3.setDepth(this.currentzindex);
     }
+    this.missileGroup.children.each((item) => {
+      item.x += 10;
+      item.rotation = Phaser.Math.DegToRad(Math.sin(time / 50) * 5 + 90);
+      item.y = item.y - Math.sin(time / 50) * 2;
+      if (item.x >= 1980) {
+        item.destroy();
+      } else {
+        if (Math.random()<.2){
+          const Smoke = this.physics.add.sprite(item.x - 15, item.y, 'smoke').setScale(0.1);
+          Smoke.alpha = .5
+          this.smokeGroup.add(Smoke);
+        }
+      }
+    });
+    this.smokeGroup.children.each(function(item) {
+      item.rotation = Phaser.Math.DegToRad(Math.cos(time / 50) * 5 + 90)
+      item.alpha -= 0.02
+      item.setScale(item.scaleX+.001)
+      if (item.alpha<=.05){
+        item.destroy()
+      }
+    }
+    )
   }
   RotateShip(degree) {
     //Rotating in radians so that it doesn't look wonky
